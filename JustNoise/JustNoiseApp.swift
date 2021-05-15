f604f15e622b05478c1eae57b0e8e3a5d4eb40a2
Michael Enger <github@michaelenger.com>
2021-05-15 15:11:45 +0100

    Initial Commit

---
+++ JustNoise/JustNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNoiseApp.swift
+//  JustNoise
+//
+//  Created by Michael Enger on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct JustNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
